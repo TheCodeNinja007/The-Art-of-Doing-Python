1fe62bbde781bce2c31f67dd6b78f598fee7eb6f
TheCodeNinja007 <mwqais01@gmail.com>
2023-12-02 19:34:36 +0000

    mph to mps conversion app

--- mph-to-mps-app.py
+++ mph-to-mps-app.py
@@ -1,1 +1,10 @@
 # Miles Per Hour to Meters Per Second Conversion App
+
+# Welcome message
+print("Welcome to the Miles Per Hour to Meters Per Second Conversion App!")
+mph = int(input("Enter the value you want to convert: "))
+
+# mps = mph / 2.237
+mps = mph * 0.4474
+mps = round(mps, 2)
+print(f"Your speed in meter per second is: {mps}")
